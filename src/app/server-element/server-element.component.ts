b433c9067b08f00cdd3c1445fa372a6afd6d225e
Cernovschi Vasilica <vasilica.cernovschi@student.usv.ro>
2023-01-27 02:23:33 +0200

    assignment solution

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -19,15 +19,7 @@ import {
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, 
-    OnChanges, 
-    DoCheck, 
-    AfterContentInit, 
-    AfterContentChecked,
-    AfterViewInit,
-    AfterViewChecked,
-    OnDestroy
-     {
+export class ServerElementComponent {
   @Input('srvElement') element: { type: string, name: string, content: string };
   @Input() name: string;
   @ViewChild('heading', {static: true}) header: ElementRef;
@@ -37,41 +29,6 @@ export class ServerElementComponent implements OnInit,
     console.log('constructor called');
    }
   
-  ngOnDestroy() {
-    console.log('ngOnDestroy called');
-  }
-
-  ngAfterViewChecked() {
-    console.log('ngAfterViewChecked called');
-  }
   
-  ngAfterViewInit() {
-    console.log('ngAfterViewInit called');
-    console.log('Text Content ' + this.header.nativeElement.textContent);
-    console.log('Text Content of paragraph' + this.paragraph.nativeElement.textContent);
-  }
-
-  ngAfterContentChecked() {
-    console.log('ngAfterContentChecked called');
-  }
-  
-  ngAfterContentInit() {
-    console.log('ngAfterContentInit called');
-  }
-
-  ngDoCheck() {
-    console.log('ngDoCheck called');
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnChanges called');
-    console.log(changes);
-  }
-
-  ngOnInit() {
-    console.log('ngOnInit called');
-    console.log('Text Content ' + this.header.nativeElement.textContent);
-    console.log('Text Content of paragraph' + this.paragraph.nativeElement.textContent);
-  }
 
 }
